08dd35d09b8867e7ae5e1f2c983174d7c701ac42
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-04-09 16:13:05 -0600

    created function to display list when clicked

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -9,15 +9,13 @@ const options = {
 	}
 };
 
-var ListRecipeURL= "https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes"
-
-
-
+var ListRecipeURL= "https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken"
+var baseURL="https://tasty.p.rapidapi.com/recipes/list?"
 // Defining Variables
 let ingredientCheckbox = document.querySelectorAll("input_checkbox_class");
-let displayIngredientsLi = document.querySelectorAll("selected-ingredients");
-let generateBtn = document.getElementById("generateBtn")
-var checkedBox= $('.checkedBox')
+let ingredientsList = $('.selectedIngredients');
+let generateBtn = document.getElementById("generateBtn");
+var checkedBox= $('.checkedBox');
 
 // Toggle light or dark mode
 if (
@@ -73,75 +71,6 @@ themeToggleBtn.addEventListener("click", function () {
   }
 });
 
-// localStorage notes delivered to me from the console:)
-// :
-// Storage
-// length
-// :
-// 0
-// [[Prototype]]
-// :
-// Storage
-// clear
-// :
-// ƒ clear()
-// getItem
-// :
-// ƒ getItem()
-// key
-// :
-// ƒ key()
-// length
-// :
-// (...)
-// removeItem
-// :
-// ƒ removeItem()
-// setItem
-// :
-// ƒ setItem()
-// constructor
-// :
-// ƒ Storage()
-// Symbol(Symbol.toStringTag)
-// :
-// "Storage"
-// get length
-// :
-// ƒ length()
-// [[Prototype]]
-// :
-// Object
-
-//ready function
-
-//checkbox for html
-{
-  /* <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-<div class="flex items-center pl-3">
-  <input id="vue-checkbox-poultry" type="checkbox" value=""
-    class="w-4 h-4 text-yellow-600 bg-gray-100 border-gray-300 rounded focus:ring-yellow-500 dark:focus:ring-yellow-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-  <label for="vue-checkbox"
-    class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Poultry</label>
-</div>
-</li> */
-}
-
-// List for HTML
-{
-  /* <ul role="list" class="marker:text-yellow-400 list-disc pl-5 space-y-3 text-slate-500">
-<li class="selected-ingredients1"></li>
-<li class="selected-ingredients"></li>
-<li class="selected-ingredients"></li>
-<li class="selected-ingredients"></li>
-<li class="selected-ingredients"></li>
-<li class="selected-ingredients"></li>
-<li class="selected-ingredients"></li>
-<li class="selected-ingredients"></li>
-<li class="selected-ingredients"></li>
-</ul> */
-}
-
 // Fetching API 
 function getAPI(){
 
@@ -156,49 +85,29 @@ function getAPI(){
 }
 getAPI();
 
-
 // Function to save checkbox values into local storage
-
 console.log(checkedBox)
+function saveIngredientsToLocalStorage(){
+  for(var i=0; i<checkedBox.length;i++)
+  var ingredientsArr=[];
+  var ingredientsCheckList={
+      "name": checkedBox[0].defaultValue,
+    }
+    ingredientsArr.push(ingredientsCheckList);
+    savedIngredientsArr=localStorage.setItem("Ingredients", JSON.stringify(ingredientsCheckList));
+    displayIngredientsLi();
+  }
 
-function saveIngredientsToLocalStorage(ingredients){
-  var savedIngredients=JSON.parse(localStorage.getItem('Ingredients')) || [];
-  savedIngredients.push(ingredients);
-  localStorage.setItem('Ingredients', JSON.stringify(savedIngredients));
+function displayIngredientsLi(){
+ingredientsList.empty();
+var savedIngredientsArr=localStorage.getItem("Ingredients");
+ingredientsHist=JSON.parse(savedIngredientsArr)
+console.log(ingredientsHist)
+
+ingredientsList.append('<li>' + ingredientsHist.name + "</li>");
 }
-
-var checkedBoxValue=checkedBox[0].checked;
-console.log(typeof checkedBoxValue)
 
 var savedIngredientsArr=[];
 
-checkedBox.on('click', function(){
-for(var i=0; checkedBox.length;i++)
-var ingredientsArr=[]
-var ingredientsCheckList={
-  "name": checkedBox[i].defaultValue,
-}
-ingredientsArr.push(ingredientsCheckList);
-savedIngredientsArr=localStorage.setItem("Ingredients", JSON.stringify(ingredientsCheckList));
-
-
-
-
-
-
-  // var checkedBoxValue=checkedBox[0].checked;
-  // var checkedBoxIngredients=checkedBox[0].defaultValue;
+checkedBox.on('click', saveIngredientsToLocalStorage);
   
-  // if(checkedBoxValue === true){
-  //     for(var i=0; i<checkedBox.length;i++){
-  //     // console.log("true")
-  //     var newcheckedBoxValue=checkedBox[i].checked
-  //     saveIngredientsToLocalStorage(newcheckedBoxValue);
-  //     localStorage.removeItem(false)
-  //   }}
-  // if (newcheckedBoxValue !== true){
-  //     // console.log('broken');
-  //     localStorage.removeItem(false);
-  //    }  
-  // }    
-  });
