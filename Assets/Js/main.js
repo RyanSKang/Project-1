35c82f8f1523cdb280398931b741348bdab6f283
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-04-10 01:35:26 -0600

    fixed local storage and removed <li> display list since its redundant

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -15,98 +15,108 @@ var baseURL="https://tasty.p.rapidapi.com/recipes/list?"
 let ingredientCheckbox = document.querySelectorAll("input_checkbox_class");
 let ingredientsList = $('.selectedIngredients');
 let generateBtn = document.getElementById("generateBtn");
-var checkedBox= $('.checkedBox');
 
 // Toggle light or dark mode
 if (
   localStorage.getItem("color-theme") === "dark" ||
   (!("color-theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  document.documentElement.classList.add("dark");
-} else {
-  document.documentElement.classList.remove("dark");
-}
-
-var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
-var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
-
-// Change the icons inside the button based on previous settings
-if (
-  localStorage.getItem("color-theme") === "dark" ||
-  (!("color-theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  themeToggleLightIcon.classList.remove("hidden");
-} else {
-  themeToggleDarkIcon.classList.remove("hidden");
-}
-
-var themeToggleBtn = document.getElementById("theme-toggle");
-
-themeToggleBtn.addEventListener("click", function () {
-  // toggle icons inside button
-  themeToggleDarkIcon.classList.toggle("hidden");
-  themeToggleLightIcon.classList.toggle("hidden");
-
-  // if set via local storage previously
-  if (localStorage.getItem("color-theme")) {
-    if (localStorage.getItem("color-theme") === "light") {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("color-theme", "light");
-    }
-
-    // if NOT set via local storage previously
+  window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
   } else {
-    if (document.documentElement.classList.contains("dark")) {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("color-theme", "light");
+    document.documentElement.classList.remove("dark");
+  }
+  
+  var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+  var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+  
+  // Change the icons inside the button based on previous settings
+  if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      themeToggleLightIcon.classList.remove("hidden");
     } else {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
+      themeToggleDarkIcon.classList.remove("hidden");
     }
+    
+    var themeToggleBtn = document.getElementById("theme-toggle");
+    
+    themeToggleBtn.addEventListener("click", function () {
+      // toggle icons inside button
+      themeToggleDarkIcon.classList.toggle("hidden");
+      themeToggleLightIcon.classList.toggle("hidden");
+      
+      // if set via local storage previously
+      if (localStorage.getItem("color-theme")) {
+        if (localStorage.getItem("color-theme") === "light") {
+          document.documentElement.classList.add("dark");
+          localStorage.setItem("color-theme", "dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+          localStorage.setItem("color-theme", "light");
+        }
+        
+        // if NOT set via local storage previously
+      } else {
+        if (document.documentElement.classList.contains("dark")) {
+          document.documentElement.classList.remove("dark");
+          localStorage.setItem("color-theme", "light");
+        } else {
+          document.documentElement.classList.add("dark");
+          localStorage.setItem("color-theme", "dark");
+        }
+      }
+    });
+    
+    // Fetching API 
+    function getAPI(){
+      
+      fetch(ListRecipeURL,options)
+      .then(function(response){
+        console.log(response);
+        return response.json();
+      })
+      .then(function(data){
+        console.log(data);
+      })
+    }
+    getAPI();
+    
+    
+    // Check boxes defined
+    let checkBoxProtein=$('.proteinCheck');
+    let checkBoxDairy=$('.dairyCheck');
+    let checkBoxVeggies=$('.veggiesCheck');
+    let checkBoxFruit=$('.fruitCheck');
+    let checkBoxSpices=$('.spicesCheck');
+    
+    // Function to save checkbox values into local storage
+    function saveIngredientsToLocalStorage(){
+      
+      const proteinCheckBox=$('.proteinCheck:checked').map(function() { return { 'name': $(this).val()} }).get();
+      localStorage.setItem("Protein", JSON.stringify(proteinCheckBox));
+      
+      const dairyCheckBox=$('.dairyCheck:checked').map(function() { return { 'name': $(this).val()} }).get();
+      localStorage.setItem("Dairy", JSON.stringify(dairyCheckBox));
+      
+      const veggiesCheckBox=$('.veggiesCheck:checked').map(function() { return { 'name': $(this).val()} }).get();
+      localStorage.setItem("Vegetables", JSON.stringify(veggiesCheckBox));
+
+      const fruitCheckBox=$('.fruitCheck:checked').map(function() { return { 'name': $(this).val()} }).get();
+      localStorage.setItem("Fruit", JSON.stringify(fruitCheckBox));
+
+      const spicesCheckBox=$('.spicesCheck:checked').map(function() { return { 'name': $(this).val()} }).get();
+      localStorage.setItem("Spices", JSON.stringify(spicesCheckBox));
+  
+    // displayIngredientsLi();
   }
-});
 
-// Fetching API 
-function getAPI(){
-
-  fetch(ListRecipeURL,options)
-  .then(function(response){
-    console.log(response);
-    return response.json();
-  })
-  .then(function(data){
-    console.log(data);
-  })
-}
-getAPI();
-
-// Function to save checkbox values into local storage
-
-var checkBoxValue=$('input[type="checkbox"]:checked').val();
-console.log(checkBoxValue)
-
-function saveIngredientsToLocalStorage(){
-  var ingredientsArr=[];
-  var ingredientsCheckList=[
-      $(this).val(),
-  ]
-    ingredientsArr.push(ingredientsCheckList);
-    savedIngredientsArr=localStorage.setItem("Ingredients", JSON.stringify(ingredientsCheckList));
-    displayIngredientsLi();
-  }
-
-function displayIngredientsLi(){
-var savedIngredientsArr=localStorage.getItem("Ingredients");
-var ingredientsHist=JSON.parse(savedIngredientsArr)
-console.log(ingredientsHist)
-
-ingredientsList.append('<li>' + ingredientsHist[0] + "</li>");
-}
-
-checkedBox.on('click', saveIngredientsToLocalStorage);
+// Event Listener
+checkBoxProtein.on('click', saveIngredientsToLocalStorage);
+checkBoxDairy.on('click', saveIngredientsToLocalStorage);
+checkBoxVeggies.on('click', saveIngredientsToLocalStorage);
+checkBoxFruit.on('click', saveIngredientsToLocalStorage);
+checkBoxSpices.on('click', saveIngredientsToLocalStorage);
   
